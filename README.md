bae570cd5277b61d69aead3e7a6ff5740c730316
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-09-22 21:02:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Sale order domain fix
+-----------------------------
+This module add a suitable filter on partner so that the user can select only customers (taking in account how odoo consider partner as customer).
+
+
---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Sale order domain fix',
+    'version': '1.0.1',
+    'author':'Soft-integration',
+    'category': 'Sale',
+    'summary': 'sale order domain fix',
+    'description': "",
+    'depends': [
+        'sale'
+    ],
+    'data': [
+        'views/sale_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
